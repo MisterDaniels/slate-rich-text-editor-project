74fba7668869768b0cbd8d762919f3d21a4adcc7
Daniel Silva <dodosilva51@gmail.com>
2020-02-22 11:31:33 -0300

    - Created more features to the tool

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -5,6 +5,9 @@ import { Value } from 'slate';
 import Icon from 'react-icons-kit';
 import { bold } from 'react-icons-kit/feather/bold';
 import { italic } from 'react-icons-kit/feather/italic';
+import { code } from 'react-icons-kit/feather/code';
+import { list } from 'react-icons-kit/feather/list';
+import { underline } from 'react-icons-kit/feather/underline';
 
 import { BoldMark, ItalicMark, FormatToolbar } from './index';
 
@@ -58,6 +61,18 @@ export default class TextEditor extends Component {
                 change.toggleMark('italic');
                 return;
             }
+            case 'c': {
+                change.toggleMark('code');
+                return;
+            }
+            case 'l': {
+                change.toggleMark('list');
+                return;
+            }
+            case 'u': {
+                change.toggleMark('underline');
+                return;
+            }
             default: {
                 return;
             }
@@ -68,10 +83,23 @@ export default class TextEditor extends Component {
     renderMark = props => {
         switch(props.mark.type) {
             case 'bold': {
-                return <BoldMark { ...props } />
+                return <BoldMark { ...props } />;
             }
             case 'italic': {
-                return <ItalicMark { ...props } />
+                return <ItalicMark { ...props } />;
+            }
+            case 'code': {
+                return <code {...props.attributes}>{props.children}</code>;
+            }
+            case 'list': {
+                return(
+                    <ul {...props.attributes}>
+                        <li>{props.children}</li>
+                    </ul>
+                );
+            }
+            case 'underline': {
+                return <u {...props.attributes}>{props.children}</u>;
             }
             default: {
                 return;
@@ -79,15 +107,49 @@ export default class TextEditor extends Component {
         }
     }
 
+    onMarkClick = (e, type) => {
+        e.preventDefault();
+
+        const { value } = this.state;
+
+        const change = value.change().toggleMark(type);
+
+        this.onChange(change);
+    }
+
     render() {
         return (
             <Fragment>
                 <FormatToolbar>
-                    <button className="tooltip-icon-button">
+                    <button 
+                        onPointerDown={(e) => this.onMarkClick(e, 'bold')}
+                        className="tooltip-icon-button"
+                    >
                         <Icon icon={bold} />
                     </button>
-                    <button className="tooltip-icon-button">
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'italic')}
+                        className="tooltip-icon-button"
+                    >
                         <Icon icon={italic} />
+                    </button>
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'code')}
+                        className="tooltip-icon-button"
+                    >
+                        <Icon icon={code} />
+                    </button>
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'list')}
+                        className="tooltip-icon-button"
+                    >
+                        <Icon icon={list} />
+                    </button>
+                    <button
+                        onPointerDown={(e) => this.onMarkClick(e, 'underline')}
+                        className="tooltip-icon-button"
+                    >
+                        <Icon icon={underline} />
                     </button>
                 </FormatToolbar>
                 <Editor 
